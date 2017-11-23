928526de6ecf1fd0edb278e5d1b02b3eb523c03c
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2017-11-23 13:16:06 +0100

    changed infrastructure a little bit

--- Proj1/Test.py
+++ Proj1/Test.py
@@ -7,7 +7,7 @@ Created on Nov 22, 2017
 from __future__ import print_function
 
 def split(X,y, n=0.8):
-    from sklearn.cross_validation import train_test_split
+    from sklearn.model_selection import train_test_split
     return train_test_split(X, y, random_state=1234, test_size=n)
 
 class Test(object):
@@ -26,6 +26,9 @@ class Test(object):
     def train(self):
         pass
     
+    def predict(self):
+        pass
+    
     def report(self):
         from sklearn import metrics
         confusion= metrics.confusion_matrix(self.test_y, self.pred_y)
@@ -39,5 +42,18 @@ class Test(object):
         print(recall)
         
     def run(self):
+        import cProfile
+        # cProfile.run('self.train()') #self.train()
+        # cProfile.runctx('self.train()', globals(), locals())
+        import cProfile, pstats, StringIO
+        pr = cProfile.Profile()
+        pr.enable()
         self.train()
+        pr.disable()
+        s = StringIO.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
         self.report()
+        
