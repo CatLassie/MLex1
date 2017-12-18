d102df1c84d1f90e56227d2f75317dddfee75e97
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2017-12-18 12:07:42 +0100

    first submission of KDD

--- Proj2/run_kdd.py
+++ Proj2/run_kdd.py
@@ -15,7 +15,7 @@ from sklearn.neighbors import KNeighborsRegressor
 import KDD
 
 def run_knn(X, y):
-    parameters= {'n_neighbors' : [290, 300, 310, 320, 330, 340, 350, 360, 400], 'weights':['uniform', 'distance'],  'p':[1, 2]}
+    parameters= {'n_neighbors' : range(1,300,10), 'weights':['uniform', 'distance'],  'p':[1, 2]}
     # parameters= {'n_neighbors' : range(4, 8), 'weights':['uniform', 'distance'],  'p':[1, 2]}
     h= Search(X, y, KNeighborsRegressor(), parameters, verbose=False)
     h.run()
@@ -31,11 +31,16 @@ def run_tree(X, y):
     
 def predict(X, y):
     o,f= KDD.load_data("test")
-    h= o['CONTROLN']
+    idx= o['CONTROLN']
+    del(o['CONTROLN'])
+    o= KDD.missing_vals(KDD.clean(KDD.xxx(o)))
     #h= Test(X, y, SVC(C=1, kernel='sigmoid', gamma=0.001))
     #h.run()
     #o['Class']= h.predict(o[features])
     #o['Class']= o['Class'].apply(lambda x: labels[x])
+    h= Test(X, y, KNeighborsRegressor(n_neighbors=290, weights='uniform', p=2))
+    o['TARGET_D']= h.predict(o)
+    o['CONTROLN']= idx
     o[list(['CONTROLN', 'TARGET_D'])].to_csv("out.kdd.csv", columns=['CONTROLN','TARGET_D'],index=False)
 
 def main():
@@ -56,21 +61,13 @@ def main():
     X= data
     
     t1= time.time()
-    run_knn(X, y)
+    # run_knn(X, y)
     # run_tree(X, y)
     t2= time.time()
     
+    predict(X, y)
+    
     print("Elapsed", (t2-t1))
-    
-    # h.report()
-    # print(h.reportTable())
-    
-#     h= Test(X, y, DecisionTreeRegressor())
-#     h.run()
-#     h= Test(X, y, KNeighborsRegressor())
-#     h.run()
-    # h.labelFun(labelFunD)
-    # h.report(fn="../Report/results/congress.knn.cm.tex")
     
 if __name__ == '__main__':
     main()
